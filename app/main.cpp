663260a518f790dd66826a236bea95c96710d911
jimmie givens <jimmiegivens@pop-os.localdomain>
2022-06-10 09:26:59 +0300

    Organised the project into folders

---
+++ app/main.cpp
@@ -0,0 +1,30 @@
+#include "Terminal.hpp"
+#include "posix/lib.hpp"
+
+#include <iostream>
+#include <cctype>
+
+#include <unistd.h>
+
+int main()
+{
+    Terminal term {};
+
+    while (true) {
+        char c {'\0'};
+        posix::read(STDIN_FILENO, &c, 1);
+
+        if (std::iscntrl(c)) {
+            std::cout << static_cast<int>(c) << "\r\n";
+        }
+        else {
+            std::cout << static_cast<int>(c) << " ('" << c << "')\r\n";
+        }
+
+        if (c == 'q') {
+            break;
+        }
+    }
+
+    return 0;
+}
