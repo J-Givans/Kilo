04850272ccf397be12e203b4645c81c54be4edb9
Jimmy Givans <owuorjimmygivansomondi@gmail.com>
2022-07-18 12:35:15 +0300

    Feature: implemented winsize_t class

--- includes/posix/lib.hpp
+++ includes/posix/lib.hpp
@@ -1,24 +1,43 @@
 #ifndef LIB_HPP
 #define LIB_HPP
 
+#include <asm-generic/ioctls.h>
 #include <cstddef>
+#include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
-#include <sys/ioctl.h>
 
-#include <system_error>
 #include <cerrno>
+#include <system_error>
+#include <utility>
 
 namespace posix {
-    void tcgetattr(int fd, termios& termios);
-    void tcsetattr(int fd, int optionalActions, termios& termios);
+void tcgetattr(int fd, termios& termios);
+void tcsetattr(int fd, int optionalActions, termios& termios);
 
-    std::size_t read(int fd, void* buffer, std::size_t count);
-    std::size_t write(int fd, void const* buffer, std::size_t count);
+std::size_t read(int fd, void* buffer, std::size_t count);
+std::size_t write(int fd, void const* buffer, std::size_t count);
+
+class winsize_t {
+public:
+    std::pair<std::size_t, std::size_t> getWindowSize() const
+    {
+        if (errno = 0; ::ioctl(STDOUT_FILENO, TIOCGWINSZ, &mWinsize) == -1) {
+            throw std::system_error { errno, std::system_category() };
+        }
+
+        return std::make_pair(mWinsize.ws_row, mWinsize.ws_col);
+    }
+
+private:
+    winsize mWinsize;
+};
+
+
 }
 
 namespace posix::constants {
-    constexpr int ssize_max = 0x7ffff000;
+constexpr int ssize_max = 0x7ffff000;
 }
 
 #endif
