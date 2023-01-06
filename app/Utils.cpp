c7acb4333dc3e55410e38f9c4281ad341a3b24d0
Jimmy Givans <owuorjimmygivansomondi@gmail.com>
2023-01-06 12:33:39 +0300

    Added: Utils header and source files.

---
+++ app/Utils.cpp
@@ -0,0 +1,83 @@
+#include "Utils.hpp"
+#include "Keys.hpp"
+
+/// @brief Perform low-level keypress handling
+int readKey()
+{
+    char c;
+    std::size_t read;
+
+    while ((read = posix::read(STDIN_FILENO, &c, 1)) != 1) {
+    }
+
+    // If we read an escape character, immediately read 2 more bytes into seq
+    // If either of these reads times out, assume the user pressed ESC and return that
+    // Otherwise, look to see if the escape sequence if an arrow key sequence
+    // If it is, return the corresponding w a s d character, else return ESC
+    if (c == '\x1b') {
+        char seq[3];
+
+        if (posix::read(STDIN_FILENO, &seq[0], 1) != 1) {
+            return '\x1b';
+        }
+        if (posix::read(STDIN_FILENO, &seq[1], 1) != 1) {
+            return '\x1b';
+        }
+
+        if (seq[0] == '[') {
+            if (seq[1] >= '0' && seq[1] <= '9') {
+                if (posix::read(STDIN_FILENO, &seq[2], 1) != 1) {
+                    return '\x1b';
+                }
+
+                if (seq[2] == '~') {
+                    switch (seq[1]) {
+                    case '1':
+                        return HOME;
+                    case '3':
+                        return DELETE;
+                    case '4':
+                        return END;
+                    case '5':
+                        return PAGE_UP;
+                    case '6':
+                        return PAGE_DOWN;
+                    case '7':
+                        return HOME;
+                    case '8':
+                        return END;
+                    }
+                }
+            } 
+            else {
+                switch (seq[1]) {
+                case 'A':
+                    return ARROW_UP;
+                case 'B':
+                    return ARROW_DOWN;
+                case 'C':
+                    return ARROW_RIGHT;
+                case 'D':
+                    return ARROW_LEFT;
+                case 'H':
+                    return HOME;
+                case 'F':
+                    return END;
+                }
+            }
+        } 
+        else if (seq[0] == 'O') {
+            switch (seq[1]) {
+            case 'H':
+                return HOME;
+            case 'F':
+                return END;
+            }
+        }
+
+        return '\x1b';
+    } 
+    else {
+        return c;
+    }
+}
---
+++ includes/Utils.hpp
@@ -0,0 +1,9 @@
+#ifndef UTILS_HPP
+#define UTILS_HPP
+
+#include <posix/lib.hpp>
+
+/// @brief Perform low-level keypress handling
+int readKey();
+
+#endif
