ddbdbc767ae9a36c3e543110f122746f4e614e61
Jimmy Givans <owuorjimmygivansomondi@gmail.com>
2023-06-16 15:09:34 +0300

    Refactor - remove deleted copy operators

--- includes/Terminal/Terminal.hpp
+++ includes/Terminal/Terminal.hpp
@@ -11,10 +11,6 @@ public:
     /// \brief Set the terminal driver into raw mode
     void enableRawMode();
 
-    /** Disable copy and move semantics */
-    Terminal(Terminal const&) = delete;
-    Terminal& operator=(Terminal const&) = delete;
-
 private:
     enum class TerminalState { Reset, Raw };
 
